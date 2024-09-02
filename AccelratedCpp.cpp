6867356e332e9da13ed6e2abcac60b15cbe4ba4d
SoonJae Han <hsj11209@gmail.com>
2024-09-02 10:26:02 +0900

    add Student Header

--- AccelratedCpp.cpp
+++ AccelratedCpp.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "Student.h"
 int main()
 {
     std::cout << "Hello World!\n";
---
+++ Student.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+class Student
+{
+private:
+	std::string name;
+	int age;
+};

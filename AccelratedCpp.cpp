8c6bb64d10ee1fbc7a2f441e439043720e136270
SoonJae Han <hsj11209@gmail.com>
2024-09-02 16:19:20 +0900

    chapter 2

--- AccelratedCpp.cpp
+++ AccelratedCpp.cpp
@@ -9,17 +9,35 @@ int main()
 
 	const std::string greeting = "Hello, " + name + "!";
 
-	const std::string spaces(greeting.size(), ' ');
-	const std::string second = "* " + spaces + " *";
+	const int pad = 1;
 
-	const std::string first(second.size(),'*');
-
+	const int rows = pad * 2 + 3;
+	const std::string::size_type cols = greeting.size() + pad * 2 + 2;
+	
 	std::cout << std::endl;
-	std::cout << first << std::endl;
-	std::cout << second << std::endl;
-	std::cout << "* " << greeting << " *" << std::endl;
-	std::cout << second << std::endl;
-	std::cout << first << std::endl;
+
+	for(int r = 0; r!= rows; ++r){
+		std::string::size_type c = 0;
+
+		while (c != cols)
+		{
+			if (r == pad + 1 && c == pad + 1) {
+				std::cout << greeting;
+				c += greeting.size();
+			}
+			else
+			{
+				if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+					std::cout << "*";
+				else
+					std::cout << " ";
+				c++;
+			}
+		}
+		std::cout << std::endl;
+	}
 	return 0;
 }
+
+
 
